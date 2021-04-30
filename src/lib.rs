6f0ba5031a375035f0b284de1f77f7086969a44f
Bogdan <bogdan.oloeriu@gmail.com>
2021-04-30 22:58:39 +0200

    fails: erdpy contract test

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,19 @@
 
 imports!();
 
-/// One of the simplest smart contracts possible,
-/// it holds a single variable in storage, which anyone can increment.
-#[elrond_wasm_derive::contract(AdderImpl)]
-pub trait Adder {
-	#[view(getSum)]
-	#[storage_get("sum")]
-	fn get_sum(&self) -> BigInt;
+#[elrond_wasm_derive::contract(CrowdfundingImpl)]
+pub trait Crowdfunding {
 
-	#[storage_set("sum")]
-	fn set_sum(&self, sum: &BigInt);
+    #[storage_set("owner")]
+    fn set_owner(&self, address: &Address);
 
-	#[init]
-	fn init(&self, initial_value: &BigInt) {
-		self.set_sum(initial_value);
-	}
+    #[view]
+    #[storage_get("owner")]
+    fn get_owner(&self) -> Address;
 
-	/// Add desired amount to the storage variable.
-	#[endpoint]
-	fn add(&self, value: &BigInt) -> SCResult<()> {
-		let mut sum = self.get_sum();
-		sum += value;
-		self.set_sum(&sum);
-
-		Ok(())
-	}
+    #[init]
+    fn init(&self) {
+        let my_address: Address = self.get_caller();
+        self.set_owner(&my_address);
+    }
 }
